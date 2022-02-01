bb64505abcd533c83c6fb24d7cb63c59d8d1e65c
Viviana <vivicalvat007@gmail.com>
2022-01-31 19:08:53 -0500

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+
+// Importar todos los routers;
+// Ejemplo: 
+// const authRouter = require('./auth.routes');
+// const continentRouter = require('./continent.routes')
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: 
+// router.use('/auth', authRouter);
+// router.use('/continent', continentRouter);
+
+
+module.exports = router;
