807f690448c62c7f28f9adf1d0a9252625cce098
Guille7580 <guilleappen@hotmail.com>
2022-02-02 20:02:54 -0300

    borre db y codigo comentado econtrollers

--- src/controllers/controllerProduct.js
+++ src/controllers/controllerProduct.js
@@ -36,53 +36,7 @@ async function getAllProductos(req, res) {
       res.send(error);
     }
   }
-// const getAllProductos = async()=>{
-//     try {
-        
-//         let getDB = await Producto.findAll()
-//         console.log('esto es productos',getDB.length)
-//         if(getDB <= 0){
-//             let get = await axios.get(`${API}`)
-//             let data = get.data
-          
-//             let showapi = await data.map((el)=>{
-//                 return{
-//                     id:el.id,
-//                     title:el.title,
-//                     price:el.price,
-//                     description:el.description,
-//                     category:el.category,
-//                     image:el.image,
-//                     rate:el.rating.rate,
-//                     count:el.rating.count,
-//                 }
-                 
-//             })
-//             let save = await data.map((el)=>{
-//                 Producto.create({
-//                     id:el.id,
-//                     title:el.title,
-//                     price:el.price,
-//                     description:el.description,
-//                     category:el.category,
-//                     image:el.image,
-//                     rate:el.rating.rate,
-//                     count:el.rating.count
-//                 })
-//             })
-//             return showapi
-//         }else{
-//             return getDB
-//         }
-        
-        
-//     } catch (err) {
-//         console.log(err)
-//         return err
-//     }
-
-// }
-
+  
 const getDetail = async(id)=>{
     id = parseInt(id)  
     let get = await Producto.findAll({
