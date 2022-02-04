0f132f7dd4024ef672a62d44304ff23ce51cbcc3
Viviana <vivicalvat007@gmail.com>
2022-02-04 12:47:11 -0500

    Adding fake data to Categoria and Usuarios

---
+++ src/data/categorias.db.js
@@ -0,0 +1,17 @@
+module.exports = [
+   {
+      nombre: "Teclados"
+   },
+   {
+      nombre: "Mouses"
+   },
+   {
+      nombre: "Auriculares"
+   },
+   {
+      nombre: "Repuestos"
+   },
+   {
+      nombre: "Cámaras"
+   }
+]
--- src/helpers/loadDb.js
+++ src/helpers/loadDb.js
@@ -1,5 +1,7 @@
-const { Producto, Usuario } = require('../db')
+const { Producto, Usuario, Categoria } = require('../db')
 const DATA_PRODUCTS = require("../data/productos.db.js");
+const DATA_USERS = require("../data/usuarios.db");
+const DATA_CATEGORIES = require("../data/categorias.db");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
 
@@ -19,47 +21,68 @@ async function LoadDb() {
     productosMaped.forEach(async (e) => {
       await Producto.findOrCreate({
         where: {
-          title: e.title,
-          price: e.price,
-          description: e.description,
-          image: e.image,
-          rate: e.rate,
-          count: e.count,
-          cantidad:e.cantidad
+          ...e
         },
       });
     });
     //console.log('DB success')
     //await Producto.bulkCreate(productosMaped, { validate: true });
     //También le creo un admin de prueba
-    let contrasena = "", avatar = "";
-    try {
-      contrasena = await bcrypt.hash("admin123", 10);
+
+    console.log("Productos cargados exitosamente");
+  } catch (error) {
+    // console.log(error);
+    console.log("No ha sido posible cargar los productos en la DB");
+  }
+
+  try {
+    const usuariosMaped = await Promise.all(DATA_USERS.map(async (e) => {
+      let contrasena = "", avatar = "";
+      contrasena = await bcrypt.hash(e.contrasena, 10);
       avatar = gravatar.url("admin@gmail.com", {
         s: "200", //size
         r: "pg", //rate
         d: "mm",
       });
-    } catch (error) {
-      console.log("No se ha podido hashear la contraseña del admin");
-    }
-    await Usuario.create({
-      "nombre": "Admin",
-      "usuario": "Admin",
-      "contrasena": contrasena,
-      "email": "admin@gmail.com",
-      "pais": "Ecuador",
-      "provincia": "Quito",
-      "direccion": "Av. Amazonica y Felipe Muñoz",
-      "telefono": "0921823478",
-      "rol": "2",
-      avatar
+
+      return {
+        nombre: e.nombre,
+        usuario: e.usuario,
+        contrasena,
+        email: e.email,
+        pais: e.pais,
+        provincia: e.provincia,
+        direccion: e.provincia,
+        telefono: e.telefono,
+        rol: e.rol,
+        avatar
+      };
+    }));
+
+    usuariosMaped.forEach(async (e) => {
+      const user = await Usuario.findOne({ where: { email: e.email } });
+      !user && await Usuario.create({
+        ...e
+      })
+    });
+
+    console.log("Usuarios cargados exitosamente");
+  } catch (error) {
+    console.log(error);
+    console.log("No ha sido posible cargar los usuarios en la DB");
+  }
+
+  try {
+    DATA_CATEGORIES.forEach(async (e) => {
+      const category = await Categoria.findOne({ where: { nombre: e.nombre } });
+
+      !category && Categoria.create(e);
     })
 
-    console.log("Productos cargados con éxito");
+    console.log("Categorías cargadas exitosamente");
   } catch (error) {
-    // console.log(error);
-    console.log("No ha sido posible cargar los productos en la DB");
+    console.log(error.message);
+    console.log("No ha sido posible cargar las categorías en la DB");
   }
 }
 
