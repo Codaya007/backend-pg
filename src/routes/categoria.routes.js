c3d0262caf81d88aa7efe8315291ad901b56afd9
Viviana <vivicalvat007@gmail.com>
2022-02-04 01:11:55 -0500

    fixing: errorHandler, statuses/ adding test requests

--- src/routes/categoria.routes.js
+++ src/routes/categoria.routes.js
@@ -1,13 +1,32 @@
 const { Router } = require('express');
-const { categoriaPost,getAllCategorias,categoriaUpdate,categoriaDelete} = require('../controllers/controllerCategorias')
+const { categoriaPost, getAllCategorias, categoriaUpdate, categoriaDelete } = require('../controllers/controllerCategorias')
 const categorRouter = Router();
 
-// Requerimos el middleware de autenticación
-const { authentication } = require("../middlewares");
+// Requerimos los middlewares de autenticación
+const { authentication, adminAuthentication } = require("../middlewares");
 
-categorRouter.post('/', categoriaPost);
+
+// @route POST categories/
+// @desc Crear una nueva categoría
+// @access Private Admin
+categorRouter.post('/', authentication, adminAuthentication, categoriaPost);
+
+
+// @route GET categories/
+// @desc Obtener todas las categorías con su información
+// @access Public
 categorRouter.get('/', getAllCategorias);
-categorRouter.post('/update', categoriaUpdate)
-categorRouter.post('/delete', categoriaDelete)
+
+
+// @route UPDATE categories/
+// @desc Eliminar una categoría por id recibido por body
+// @access Private Admin
+categorRouter.put('/update', authentication, adminAuthentication, categoriaUpdate);
+
+
+// @route DELETE categories/
+// @desc Eliminar una categoría por id recibido por body
+// @access Private Admin
+categorRouter.delete('/delete', authentication, adminAuthentication, categoriaDelete);
 
 module.exports = categorRouter;
