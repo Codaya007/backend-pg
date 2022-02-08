42847f18f752d2f0d144500a20a983c94099f4f6
Viviana <vivicalvat007@gmail.com>
2022-02-08 18:24:42 -0500

    Añadiendo validaciones y autenticacion a los pagos

--- src/controllers/controllerPagos.js
+++ src/controllers/controllerPagos.js
@@ -1,32 +1,45 @@
-const { Sequelize } = require("sequelize");
-const Op = Sequelize.Op;
 const Stripe = require("stripe");
 const { CLAVE_PRIVADA } = process.env;
 const stripe = new Stripe(CLAVE_PRIVADA);
+const { Pedido } = require("../db");
+const { NOMBRE_ECOMMERCE } = process.env;
 
-async function pagosPost (req, res) {
-    // you can get more data to find in a database, and so on
-    const { id, amount } = req.body;
-    console.log(req.body)
-    try {
-      const payment = await stripe.paymentIntents.create({
-        amount,
-        currency: "USD",
-        description: "Gaming Keyboard",
-        payment_method: id,
-        confirm: true, //confirm the payment at the same time
-      });
-  
-      console.log(payment);
-  
-      return res.status(200).json({ message: "Successful Payment" });
-    } catch (error) {
-      console.log(error);
-      return res.json({ message: error.raw.message });
-    }
-  };
+async function pagosPost(req, res, next) {
+  // Destructuramos los atributos que recibimos por body
+  const { transaccionId, pedidoId } = req.body;
+
+  // console.log(req.body)
+  try {
+    // Traemos el pedido para generar una descripción y obtener el precio de la transacción
+    let pedido = await Pedido.findByPk(pedidoId);
+
+    if (!pedido) return next({ status: 400, message: "El id del pedido no es válido" });
+    // Si hay un id lo pasamos a json
+    pedido = pedido.toJSON();
+
+    // Generamos una descripción
+    let description = `Deducción por compra id ${pedido.id} realizada en ${NOMBRE_ECOMMERCE}. Gracias por su compra`;
+
+    // Creamos un nuevo pago
+    const payment = await stripe.paymentIntents.create({
+      amount: pedido.total,
+      currency: "USD",
+      description,
+      payment_method: transaccionId,
+      confirm: true, //confirmamos el pago
+    });
+
+    console.log(payment);
+
+    return res.status(200).json({ message: "Successful Payment" });
+  } catch (error) {
+
+    console.log(error.raw.message);
+    return next({ status: 400, message: error.raw.message });
+  }
+};
 
 
 module.exports = {
-    pagosPost
- }
+  pagosPost
+}
--- src/routes/pagos.routes.js
+++ src/routes/pagos.routes.js
@@ -1,17 +1,15 @@
 const { Router } = require('express');
-const {pagosPost } = require('../controllers/controllerPagos')
+const { pagosPost } = require('../controllers/controllerPagos')
 const pagoRouter = Router();
 
 // Requerimos los middlewares de autenticación
-const { authentication, adminAuthentication } = require("../middlewares");
+const { authentication } = require("../middlewares");
 
 
 // @route POST categories/
 // @desc Crear una nueva categoría
 // @access Private Admin
 //pagoRouter.post('/', authentication, adminAuthentication, pagosPost);
-pagoRouter.post('/', pagosPost);
-
-
+pagoRouter.post('/', authentication, pagosPost);
 
 module.exports = pagoRouter;
