94cd12304e3859e5ed907421bcdfca6370ebe41c
Guille7580 <guilleappen@hotmail.com>
2022-02-10 06:24:14 -0300

    acomodando

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { PORT } = process.env;
 const LoadDb = require('./src/helpers/loadDb')
 
 // Syncing all the models at once.
-conn.sync({ force: true })
+conn.sync({ force: false })
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Listening at port ${PORT}`);
