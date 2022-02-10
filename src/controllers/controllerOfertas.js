4b2083c56d4f3dc6b024891e475a264849081edc
Viviana <vivicalvat007@gmail.com>
2022-02-10 16:19:33 -0500

    Arreglando rutas de POST y PUT ofertas, Añadiendo  rutas PUT para agregar y remover productos de una oferta

--- src/controllers/controllerOfertas.js
+++ src/controllers/controllerOfertas.js
@@ -1,9 +1,15 @@
 const { ACTIVA } = require("../data/constantes");
 const { Ofertas, OfertaProducto, Producto } = require('../db')
-
+const { Sequelize } = require("sequelize");
+const Op = Sequelize.Op;
 
 const getOfertaById = async (id) => {
-  let oferta = await Ofertas.findByPk(id);
+  let oferta = await Ofertas.findByPk(id, {
+    include: {
+      model: OfertaProducto,
+      attributes: ["productoId", "cantidad"]
+    },
+  });
 
   if (!oferta) return { error: { status: 404, message: "Oferta no encontrada" } };
 
@@ -12,7 +18,25 @@ const getOfertaById = async (id) => {
 
 
 const getAllOfertas = async () => {
-  let get = await Ofertas.findAll({ include: OfertaProducto, where: { estado: ACTIVA } });
+  let get = await Ofertas.findAll({
+    include: {
+      model: OfertaProducto,
+      attributes: ["productoId", "cantidad"]
+    }
+  });
+  console.log(get);
+  return get
+}
+
+
+const getOfertasActivas = async () => {
+  let get = await Ofertas.findAll({
+    include: {
+      model: OfertaProducto,
+      attributes: ["productoId", "cantidad"]
+    },
+    where: { estado: ACTIVA }
+  });
   console.log(get);
   return get
 }
@@ -72,23 +96,128 @@ const createOferta = async (titulo, descripcion, porcentajeDescuento, productos)
   }
 }
 
-const updateOferta = async (id, titulo, descripcion, porcentajeDescuento, estado, cantidad) => {
+const updateOferta = async (id, titulo, descripcion, porcentajeDescuento, estado, productos) => {
   try {
+
+    let oferta = await Ofertas.findOne({ where: { titulo } });
+
+    if (oferta) return { error: { status: 400, message: "Ya existe una oferta con ese nombre" } };
+
     let updated = await Ofertas.update(
       {
         titulo,
         descripcion,
         porcentajeDescuento,
         estado,
-        cantidad
       },
-      { where: { id } })
+      { where: { id } });
+
+    if (updated[0] === 0) return { error: { status: 404, message: "Oferta no encontrada" } };
+
+    if (productos) {
+      // Valido que todos los productos que se van a poner en promoción existan y que alcance la cantidad dada
+      productos = await Promise.all(productos.map(async (prod) => {
+        let producto = await Producto.findByPk(prod.id);
+        if (!producto) return;
+
+        producto = producto.toJSON();
+        if (producto.cantidad < 1) return;
+
+        // Si hay menos productos en stock de los que estan para oferta, le entrego todo el stock
+        prod.cantidad = prod.cantidad <= producto.cantidad ? prod.cantidad : producto.cantidad;
+
+        return prod;
+      }));
+
+      productos = productos.filter(prod => prod);
+
+      if (!productos.length) return {
+        error: { status: 400, message: "No hay suficientes productos en stock" }
+      };
+
+      // Creo las relaciones entre oferta - producto con la instancia OfertaProducto
+      await Promise.all(productos.map(async (prod) => {
+        await OfertaProducto.update({
+          cantidad: prod.cantidad
+        }, { where: { productoId: prod.id } });
+      }));
+    }
+
     return { message: "Actualización exitosa" };
   } catch (error) {
     console.log(error);
     return { error: {} };
   }
 }
+
+
+const addProducto = async (ofertaId, producto) => {
+
+  try {
+    const oferta = await Ofertas.findByPk(ofertaId);
+    if (!oferta) return { error: { status: 404, message: "Oferta no encontrada" } };
+
+    const productoExists = await Producto.findByPk(producto.id);
+    if (!productoExists) return { error: { status: 404, message: "Producto no encontrado" } };
+
+    const exists = await OfertaProducto.findOne({
+      where: {
+        [Op.and]: [
+          {
+            productoId: producto.id
+          },
+          {
+            ofertaId
+          }
+        ]
+      }
+    });
+
+    if (exists) return { error: { status: 400, message: "El producto ya está incluido en la oferta actual" } };
+
+    const created = await OfertaProducto.create({
+      productoId: producto.id,
+      cantidad: producto.cantidad || 5,
+      ofertaId
+    });
+
+    return created;
+  } catch (error) {
+    console.log(error);
+    return { error: {} };
+  }
+}
+
+
+const removeProducto = async (ofertaId, productoId) => {
+  try {
+    const oferta = await Ofertas.findByPk(ofertaId);
+
+    console.log(oferta);
+    if (!oferta) return { error: { status: 404, message: "Oferta no encontrada" } };
+
+    const exists = await OfertaProducto.destroy({
+      where: {
+        [Op.and]: [
+          {
+            productoId
+          },
+          {
+            ofertaId
+          }
+        ]
+      }
+    });
+
+    if (!exists) return { error: { status: 404, message: "Producto no encontrado en la oferta actual" } };
+
+    return;
+  } catch (error) {
+    console.log(error);
+    return { error: {} };
+  }
+}
+
 
 const deleteOferta = async (id) => {
   try {
@@ -98,7 +227,16 @@ const deleteOferta = async (id) => {
 
     if (!deleted) return { error: { status: 404, message: "Id de la oferta no es válido" } };
 
-    return;
+    // Traigo todas las ofertasProducto
+    let ofertaProductos = await OfertaProducto.findAll({ where: { ofertaId: id } });
+
+    // Voy por cada ofertaProducto y las elimino
+    await Promise.all(ofertaProductos.forEach(async (el) => {
+      el = el.toJSON();
+      await OfertaProducto.destroy({ where: { id: el.id } });
+    }));
+
+    return {};
   } catch (error) {
     console.log(error);
     return { error: {} }
@@ -111,5 +249,8 @@ module.exports = {
   createOferta,
   updateOferta,
   deleteOferta,
-  getOfertaById
+  getOfertaById,
+  getOfertasActivas,
+  addProducto,
+  removeProducto
 }
--- src/routes/oferta.routes.js
+++ src/routes/oferta.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
-const { getAllOfertas, createOferta, updateOferta, deleteOferta, getOfertaById } = require('../controllers/controllerOfertas');
+const { getAllOfertas, createOferta, updateOferta, deleteOferta, getOfertaById, getOfertasActivas, removeProducto, addProducto } = require('../controllers/controllerOfertas');
 
 
 const offersRouter = Router();
+
+// Requerimos los middlewares de autenticación
+const { authentication, adminAuthentication } = require("../middlewares");
+
 
 offersRouter.get('/', async (req, res, next) => {
      let result = await getAllOfertas();
@@ -11,6 +15,16 @@ offersRouter.get('/', async (req, res, next) => {
 
      res.send(result);
 })
+
+
+offersRouter.get('/active', async (req, res, next) => {
+     let result = await getOfertasActivas();
+
+     if (result.error) return next(result.error);
+
+     res.send(result);
+})
+
 
 offersRouter.get('/:id', async (req, res, next) => {
      const { id } = req.params;
@@ -22,7 +36,8 @@ offersRouter.get('/:id', async (req, res, next) => {
      res.send(result);
 })
 
-offersRouter.post('/', async (req, res, next) => {
+
+offersRouter.post('/', authentication, adminAuthentication, async (req, res, next) => {
      const { titulo, descripcion, porcentajeDescuento, productos } = req.body;
 
      let result = await createOferta(titulo, descripcion, porcentajeDescuento, productos);
@@ -32,18 +47,47 @@ offersRouter.post('/', async (req, res, next) => {
      res.json(result);
 })
 
-offersRouter.put('/:id', async (req, res, next) => {
-     const { titulo, descripcion, porcentajeDescuento, estado, cantidad } = req.body;
+
+// Esta ruta me permite cambiar el titulo, descripcion, porcentajeDescuento o estado
+// De los productos de esta oferta no me deja modificar mas que la cantidad
+// Si no le paso productos funciona igual
+offersRouter.put('/:id', authentication, adminAuthentication, async (req, res, next) => {
+     const { titulo, descripcion, porcentajeDescuento, estado, productos } = req.body;
      const { id } = req.params;
 
-     let result = await updateOferta(id, titulo, descripcion, porcentajeDescuento, estado, cantidad);
+     let result = await updateOferta(id, titulo, descripcion, porcentajeDescuento, estado, productos);
 
      if (result.error) return next(result.error);
 
      res.json(result);
 })
 
-offersRouter.delete('/:id', async (req, res) => {
+
+offersRouter.put('/add/:ofertaId', authentication, adminAuthentication, async (req, res, next) => {
+     const { producto } = req.body;
+     const { ofertaId } = req.params;
+
+     let result = await addProducto(ofertaId, producto);
+
+     if (result.error) return next(result.error);
+
+     res.json(result);
+})
+
+
+offersRouter.put('/remove/:ofertaId', authentication, adminAuthentication, async (req, res, next) => {
+     const { productoId } = req.body;
+     const { ofertaId } = req.params;
+
+     let destroy = await removeProducto(ofertaId, productoId);
+
+     if (destroy) return next(destroy.error);
+
+     res.status(204).end();
+})
+
+
+offersRouter.delete('/:id', authentication, adminAuthentication, async (req, res) => {
      const { id } = req.params;
 
      let destroy = await deleteOferta(id);
